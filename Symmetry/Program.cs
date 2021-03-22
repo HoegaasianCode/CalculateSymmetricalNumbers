76e0bcf4c5ea2215c80fb22a2f68013f2359de49
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-22 13:20:25 +0100

    Initial commit

---
+++ Symmetry/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Symmetry
+{
+    class Program
+    {
+        // https://edabit.com/challenge/H77JNgLtEnQcX5hcu
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            SymmetryCalc symmetry = new SymmetryCalc(14441);
+            symmetry.ConvertToArray();
+            symmetry.ReverseArray();
+            symmetry.IsSymmetrical();
+        }
+    }
+}
---
+++ Symmetry/SymmetryCalc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Symmetry
+{
+    public class SymmetryCalc
+    {
+        private readonly int _int;
+        private int[] _array;
+        private int[] _reversedArray;
+
+        public SymmetryCalc(int integer)
+        {
+            _int = integer;
+        }
+
+        public void ConvertToArray()
+        {
+            _array = _int.ToString().ToCharArray().Select(x =>
+            (int)Char.GetNumericValue(x)).ToArray();
+        }
+
+        public void ReverseArray()
+        {
+            _reversedArray = _array.Reverse().ToArray();
+        }
+
+        public void IsSymmetrical()
+        {
+            if (Enumerable.SequenceEqual(_array, _reversedArray)) Console.WriteLine("Symmetrical");
+            else Console.WriteLine("Nonsymmetrical");
+        }
+    }
+}
+
